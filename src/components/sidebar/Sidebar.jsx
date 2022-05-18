ee68a880e0ba1138849a7ba9a30f8972a87c154b
AnkurDesai11 <60006478+AnkurDesai11@users.noreply.github.com>
2022-05-19 01:32:16 +0530

    added material ui and icnos to sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,21 +1,53 @@
 import "./sidebar.scss"
+import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
+import GridViewIcon from '@mui/icons-material/GridView';
+import GroupOutlinedIcon from '@mui/icons-material/GroupOutlined';
+import StorageRoundedIcon from '@mui/icons-material/StorageRounded';
+import QueryStatsRoundedIcon from '@mui/icons-material/QueryStatsRounded';
+import NotificationImportantIcon from '@mui/icons-material/NotificationImportant';
+import HealthAndSafetyOutlinedIcon from '@mui/icons-material/HealthAndSafetyOutlined';
+import ArticleIcon from '@mui/icons-material/Article';
+import SettingsIcon from '@mui/icons-material/Settings';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded';
 
 const Sidebar = () => {
     return (
         <div className="sidebar">
             <div className="top">
-                <span className="logo"><span className="innerLogo">db</span>Admin</span>
+                <span className="logo"><span className="innerLogo"><AdminPanelSettingsIcon />db</span>Admin</span>
             </div>
             <hr />
             <div className="center">
                 <ul>
-                    <li>View Users</li>
+                    <li><GridViewIcon className="icon" /><span>Dashboard</span></li>
                 </ul>
                 <ul>
-                    <li>Add User</li>
+                    <li><GroupOutlinedIcon className="icon" /><span>Users</span></li>
                 </ul>
                 <ul>
-                    <li>View Databases</li>
+                    <li><StorageRoundedIcon className="icon" /><span>Databases</span></li>
+                </ul>
+                <ul>
+                    <li><QueryStatsRoundedIcon className="icon" /><span>Stats</span></li>
+                </ul>
+                <ul>
+                    <li><NotificationImportantIcon className="icon" /><span>Notifications</span></li>
+                </ul>
+                <ul>
+                    <li><HealthAndSafetyOutlinedIcon className="icon" /><span>System Health</span></li>
+                </ul>
+                <ul>
+                    <li><ArticleIcon className="icon" /><span>Logs</span></li>
+                </ul>
+                <ul>
+                    <li><SettingsIcon className="icon" /><span>Settings</span></li>
+                </ul>
+                <ul>
+                    <li><AccountCircleOutlinedIcon className="icon" /><span>Profile</span></li>
+                </ul>
+                <ul>
+                    <li><LogoutRoundedIcon className="icon" /><span>Logout</span></li>
                 </ul>
             </div>
             <div className="bottom">Theme</div>
