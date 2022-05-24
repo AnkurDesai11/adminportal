d3c2bc9da291224db9b8061544d83546f27ad6b0
AnkurDesai11 <60006478+AnkurDesai11@users.noreply.github.com>
2022-05-25 01:35:01 +0530

    done featured started chart component

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -13,7 +13,18 @@ const Featured = () => {
                     <CircularProgressbar value={70} text={"70"} strokeWidth={3} />
                 </div>
                 <p className="title">Ratio of entries in MySQL and MongoDB</p>
-                <p className="description">Some entries may be replicated across databases if migrated</p>
+                <p className="description">Some entries may be common across databases if migrated</p>
+                <p className="description">Duplicted entries may not necessarily have all details same in other database, Migrated entries will</p>
+                <div className="numbers">
+                    <div className="item mysql">
+                        <div className="itemTitle">MySql</div>
+                        <div className="itemValue">11</div>
+                    </div>
+                    <div className="item mongo">
+                        <div className="itemTitle">MongoDB</div>
+                        <div className="itemValue">15</div>
+                    </div>
+                </div>
             </div>
         </div>
     )
