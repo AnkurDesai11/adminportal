862eec864204fdc56e4be383f5fd0dd4c8606dd1
AnkurDesai11 <60006478+AnkurDesai11@users.noreply.github.com>
2022-05-29 23:16:13 +0530

    fixed links

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -15,31 +15,31 @@ const Sidebar = () => {
     return (
         <div className="sidebar">
             <div className="top">
-                <span className="logo"><span className="innerLogo"><AdminPanelSettingsIcon />db</span>Admin</span>
+                <span className="logo"><a href="/"><span className="innerLogo"><AdminPanelSettingsIcon />db</span>Admin</a></span>
             </div>
             <hr />
             <div className="center">
                 <ul>
                     <p className="title">Overview</p>
-                    <li><GridViewIcon className="icon" /><span>Dashboard</span></li>
+                    <li><GridViewIcon className="icon" /><span><a href="/">Dashboard</a></span></li>
                     <p className="title">Details</p>
-                    <li><GroupOutlinedIcon className="icon" /><span>Users</span></li>
+                    <li><GroupOutlinedIcon className="icon" /><span><a href="/users">Users</a></span></li>
 
-                    <li><StorageRoundedIcon className="icon" /><span>Databases</span></li>
+                    <li><StorageRoundedIcon className="icon" /><span><a href="/databases">Databases</a></span></li>
 
-                    <li><QueryStatsRoundedIcon className="icon" /><span>Stats</span></li>
+                    <li><QueryStatsRoundedIcon className="icon" /><span><a href="/stats">Stats</a></span></li>
                     <p className="title">Service</p>
-                    <li><NotificationImportantIcon className="icon" /><span>Notifications</span></li>
+                    <li><NotificationImportantIcon className="icon" /><span><a href="/notifications">Notifications</a></span></li>
 
-                    <li><HealthAndSafetyOutlinedIcon className="icon" /><span>System Health</span></li>
+                    <li><HealthAndSafetyOutlinedIcon className="icon" /><span><a href="/health">System Health</a></span></li>
 
-                    <li><ArticleIcon className="icon" /><span>Logs</span></li>
+                    <li><ArticleIcon className="icon" /><span><a href="/logs">Logs</a></span></li>
                     <p className="title">User</p>
-                    <li><SettingsIcon className="icon" /><span>Settings</span></li>
+                    <li><SettingsIcon className="icon" /><span><a href="/settings">Settings</a></span></li>
 
-                    <li><AccountCircleOutlinedIcon className="icon" /><span>Profile</span></li>
+                    <li><AccountCircleOutlinedIcon className="icon" /><span><a href="/profile">Profile</a></span></li>
 
-                    <li><LogoutRoundedIcon className="icon" /><span>Logout</span></li>
+                    <li><LogoutRoundedIcon className="icon" /><span><a href="/logout">Logout</a></span></li>
                 </ul>
             </div>
             <div className="bottom">
@@ -47,7 +47,7 @@ const Sidebar = () => {
                 <div className="colorOption"></div>
                 <div className="colorOption"></div>
             </div>
-        </div>
+        </div >
     )
 }
 
