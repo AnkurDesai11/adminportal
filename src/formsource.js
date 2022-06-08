23561526c7d1872994d8fdfb653c0970a083dff3
AnkurDesai11 <60006478+AnkurDesai11@users.noreply.github.com>
2022-06-08 16:05:41 +0530

    added new user input form

---
+++ src/formsource.js
@@ -0,0 +1,32 @@
+export const newUserForm = [
+    {
+        id: 1,
+        label: "First name",
+        type: "text",
+        placeholder: "John"
+    },
+    {
+        id: 2,
+        label: "Last name",
+        type: "text",
+        placeholder: "Doe"
+    },
+    {
+        id: 3,
+        label: "First name",
+        type: "text",
+        placeholder: "John"
+    },
+    {
+        id: 4,
+        label: "Username",
+        type: "text",
+        placeholder: "john_doe"
+    },
+    {
+        id: 5,
+        label: "Email Id",
+        type: "email",
+        placeholder: "john.doe@mail.com"
+    },
+];
--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
 import "./new.scss"
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
+import { PhoneInput } from "react-contact-number-input";
+import { useState } from "react";
+import AddPhotoAlternateRoundedIcon from '@mui/icons-material/AddPhotoAlternateRounded';
 
-const New = () => {
+const New = ({ fields, title }) => {
+    const [value, setValue] = useState()
+    const [file, setFile] = useState()
+    console.log(value)
+    console.log(file)
     return (
-        <div>New</div>
+        <div className="new">
+            <link rel="stylesheet" href="" />
+            <Sidebar />
+            <div className="newContainer">
+                <Navbar />
+                <div className="top"><h2>{title}</h2></div>
+                <div className="bottom">
+                    <div className="left">
+                        <img src={file ? URL.createObjectURL(file) : "https://static.vecteezy.com/system/resources/previews/000/378/452/original/edit-profile-vector-icon.jpg"} alt="" />
+                    </div>
+                    <div className="right">
+                        <form action="">
+                            <div className="formInput">
+                                <label htmlFor="file">
+                                    Upload Profile Image:<AddPhotoAlternateRoundedIcon className="ImageIcon" />
+                                </label>
+                                <input type="file" id="file" onChange={e => setFile(e.target.files[0])} style={{ display: "none" }} />
+                            </div>
+                            {fields.map((field) => (
+                                <div className="formInput">
+                                    <label htmlFor="">{field.label}</label>
+                                    <input type={field.type} placeholder={field.placeholder} />
+                                </div>))}
+                            {title === "Add New User" &&
+                                <div className="formInputPhone" style={{ color: "dimgray" }}>
+                                    <label htmlFor="">Phone</label>
+                                    <PhoneInput
+                                        disabled=""
+                                        containerClass=""
+                                        onChange={setValue}
+                                        placeholder="Enter Contact Number"
+                                    />
+                                </div>
+                            }
+                            <button>Save</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
