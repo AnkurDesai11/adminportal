e4abfcbb75fd746ebf594f55b36088c541497000
AnkurDesai11 <60006478+AnkurDesai11@users.noreply.github.com>
2022-07-18 18:23:48 +0530

    implemented login with embedded okta signin widget

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import "./login.scss"
+import Navbar from '../../components/navbar/Navbar'
+import Sidebar from '../../components/sidebar/Sidebar'
+import { Redirect } from 'react-router-dom';
+import OktaSignInWidget from '../../components/OktaSignInWidget/OktaSignInWidget';
+import { useOktaAuth } from '@okta/okta-react';
 
-const Login = () => {
-    return (
-        <div>Login</div>
-    )
-}
+const Login = ({ config }) => {
+    const { oktaAuth, authState } = useOktaAuth();
+    const onSuccess = (tokens) => {
+        oktaAuth.handleLoginRedirect(tokens);
+    };
 
-export default Login
+    const onError = (err) => {
+        console.log('Sign in error:', err);
+    };
+
+    if (!authState) {
+        return <div>Loading ... </div>;
+    }
+
+    return authState.isAuthenticated ?
+        <Redirect to={{ pathname: '/' }} /> :
+        <div className="login">
+            <Sidebar />
+            <div className="loginContainer">
+                <Navbar />
+                <OktaSignInWidget className="loginWidget" config={config} onSuccess={onSuccess} onError={onError} />
+            </div>
+        </div>;
+};
+
+export default Login;
